76f6e5107f5ca7f76cb77b178a863aadeb5b0bc7
Aleksandr <inimitable73@gmail.com>
2023-01-13 15:28:39 +0400

    create modules

--- app/src/main/java/com/example/weathermvvm/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weathermvvm/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.weathermvvm
+package com.example.weathermvvm.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weathermvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
