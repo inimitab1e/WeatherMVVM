707f01d139422fa56b5572ea83b14dc1c5d7983e
Aleksandr <inimitable73@gmail.com>
2023-01-13 22:39:00 +0400

    add fragments, start with navigation

---
+++ app/src/main/java/com/example/weathermvvm/presentation/ui/SearchWeatherFragment.kt
@@ -0,0 +1,30 @@
+package com.example.weathermvvm.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weathermvvm.R
+import com.example.weathermvvm.databinding.FragmentSearchWeatherBinding
+
+class SearchWeatherFragment : Fragment() {
+    private var binding: FragmentSearchWeatherBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchWeatherBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
