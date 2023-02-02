f7419bd782d4f8ede2c9deaed8f3c79d50678f78
Aleksandr <inimitable73@gmail.com>
2023-02-02 18:46:52 +0400

    added preference fragment

--- app/src/main/java/com/example/weathermvvm/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/weathermvvm/presentation/ui/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.appcompat.app.ActionBar
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.weathermvvm.R
 import com.example.weathermvvm.databinding.ActivityMainBinding
+import com.example.weathermvvm.presentation.ui.settings.SettingsFragment
 import com.example.weathermvvm.utils.NetworkConnection
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -35,6 +37,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private fun setupViews() {
         setSupportActionBar(binding.myToolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.home_page) as NavHostFragment
@@ -48,8 +51,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     private fun checkConnectivity() {
-        connectivity.observe(this){
-                isConnected ->
+        connectivity.observe(this) { isConnected ->
              if (!isConnected) {
                 with(binding) {
                     bottomNavigationView.isVisible = false
@@ -60,13 +62,17 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                      bottomNavigationView.isVisible = true
                      badNetworkConnectionPage.isGone = true
                  }
-             }
+            }
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_settings -> {
-            // User chose the "Settings" item, show the app settings UI...
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.home_page, SettingsFragment())
+                .commit()
+            binding.bottomNavigationView.isGone = true
             true
         }
         else -> super.onOptionsItemSelected(item)
---
+++ app/src/main/java/com/example/weathermvvm/presentation/ui/settings/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.weathermvvm.presentation.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.example.weathermvvm.R
+
+class SettingsFragment: PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preference, rootKey)
+
+    }
+}
