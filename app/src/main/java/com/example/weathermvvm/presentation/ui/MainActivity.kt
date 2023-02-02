991e15260185f2d982ed766107e6ccca99c57b5c
Aleksandr <inimitable73@gmail.com>
2023-02-02 21:06:28 +0400

    impl'ed actionbar home btn logic

--- app/src/main/java/com/example/weathermvvm/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/weathermvvm/presentation/ui/MainActivity.kt
@@ -4,20 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
+import androidx.activity.OnBackPressedCallback
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.weathermvvm.R
 import com.example.weathermvvm.databinding.ActivityMainBinding
+import com.example.weathermvvm.presentation.ui.search.SearchWeatherFragment
 import com.example.weathermvvm.presentation.ui.settings.SettingsFragment
 import com.example.weathermvvm.utils.NetworkConnection
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -37,17 +37,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private fun setupViews() {
         setSupportActionBar(binding.myToolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.home_page) as NavHostFragment
         navConrtoller = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navConrtoller)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.action_bar_menu, menu)
-        return true
     }
 
     private fun checkConnectivity() {
@@ -66,15 +61,33 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.action_settings -> {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.home_page, SettingsFragment())
-                .commit()
-            binding.bottomNavigationView.isGone = true
-            true
-        }
+        R.id.action_settings -> handleOnSettingsButtonClick()
+        android.R.id.home -> handleOnHomeActionbarButtonClick()
         else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun handleOnSettingsButtonClick(): Boolean {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.home_page, SettingsFragment())
+            .commit()
+        binding.bottomNavigationView.isGone = true
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        return true
+    }
+
+    private fun handleOnHomeActionbarButtonClick(): Boolean {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.home_page, SearchWeatherFragment())
+            .commit()
+        binding.bottomNavigationView.isVisible = true
+        return true
     }
 }
