5e20cc52f524255d9e25eaa5d26c787f9369c2ec
Aleksandr <inimitable73@gmail.com>
2023-02-06 19:48:39 +0400

    bottomNavigationBar visibility logic fixed

--- app/src/main/java/com/example/weathermvvm/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/weathermvvm/presentation/ui/MainActivity.kt
@@ -4,25 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.preference.PreferenceManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.weathermvvm.R
 import com.example.weathermvvm.databinding.ActivityMainBinding
-import com.example.weathermvvm.presentation.ui.search.SearchWeatherFragment
-import com.example.weathermvvm.presentation.ui.settings.SettingsFragment
 import com.example.weathermvvm.utils.NetworkConnection
 import com.example.weathermvvm.utils.SettingsSharedPreferences
 import com.example.weathermvvm.utils.StringConstants
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -38,18 +32,26 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setupViews()
+        setupNavigation()
         checkAppThemeState()
         checkConnectivity()
     }
 
-    private fun setupViews() {
+    private fun setupNavigation() {
         setSupportActionBar(binding.myToolbar)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.home_page) as NavHostFragment
         navConrtoller = navHostFragment.navController
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navConrtoller)
+
+        navConrtoller.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.settingsFragment) {
+                binding.bottomNavigationView.isGone = true
+            } else {
+                binding.bottomNavigationView.isVisible = true
+            }
+        }
     }
 
     private fun checkAppThemeState() {
--- app/src/main/java/com/example/weathermvvm/presentation/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/weathermvvm/presentation/ui/settings/SettingsFragment.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.example.weathermvvm.R
 
-class SettingsFragment: PreferenceFragmentCompat() {
-
+class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preference, rootKey)
     }
